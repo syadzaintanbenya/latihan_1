8421131caff65de05d8aebe5ab0fe7e3265c860e
syadzaintanbenya <91009370+syadzaintanbenya@users.noreply.github.com>
2022-11-09 23:09:52 +0700

    Update VirtualDemo.java

--- VirtualDemo.java
+++ VirtualDemo.java
@@ -1,9 +1,20 @@
+package tugas1;
+
 public class VirtualDemo {
     public static void main(String []args) {
-        Gaji s = new Gaji("Wahyu", "KUBAR", 3, 500.00);
-        Pegawai e = new Gaji("ini nama", "Samarinda", 2, 2500.00);
-        System.out.println("memanggil mailCheck berdasarkan refensi gaji--");
+        
+       
+        Gaji s = new Gaji ("Wahyu", "KUBAR", 3, 500.00); 
+       
+    
+        Pegawai e = new Gaji("ini nama", "Samarinda", 2,  2500.00);//upcasting karena dari class gaji akan mengakses method dari class pegawai
+        
+        
+        System.out.println("Memanggil mailCheck Berdasarkan Refensi Gaji--");
+        s.mailCheck();
 
+        System.out.println("\nMemanggil mailCheck Berdasarkan Refensi Pegawai--");
         e.mailCheck();
     }
 }
+
