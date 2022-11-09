d3166e6b959ecae3601e29f51c45336a6b5b5399
syadzaintanbenya <91009370+syadzaintanbenya@users.noreply.github.com>
2022-11-09 23:08:11 +0700

    Update Gaji.java

--- Gaji.java
+++ Gaji.java
@@ -1,29 +1,33 @@
-public class Gaji extends Pegawai{
+package tugas1;
 
-        private double salary; // gaji tahunan
-        public Gaji (String name, String address, int number, double 
-        salary ) {
-            super (name, address, number);
-            setSalary (salary);
-        }
-    public void mailCheck(){
-        System.out.println("memeriksa kelas gaji dalam surat");
-        System.out.println("surat tertuju untuk " + getName () + "dengan gaji" + salary);
-    }
-    public double getSalary (){
-        return salary;
-    }
-    public void setSalary (double newSalary){
-        if (newSalary >= 0.0) {
-            salary = newSalary;
-        }
-    }
-    public double computePay(){
-        System.out.println("menghitung pembayaran gaji untuk " + getName());
-        return salary/52;
-    
-    }
-    
+//class pegawai mewariskan sifat ke class Gaji
+//Polymorphisme(memiliki banyak bentuk)
 
-    
+public class Gaji extends Pegawai {
+    private double salary; // gaji tahunan
+    public Gaji (String name, String address, int number, double 
+    salary ) {
+        super (name, address, number);
+        setSalary (salary);
+    }
+
+  /* Polymorphisme upcasting karena ada hubungan antara subclass dengan superclass
+  yang nantinya hasil dari outputnya berbeda dengan inputannya dari class gaji dengan class pegawai(superclass) */
+public void mailCheck(){ 
+    System.out.println("Memeriksa Kelas Gaji dalam Surat");
+    System.out.println("Surat Tertuju untuk " + getName () + "dengan Gaji" +  salary);
+}
+public double getSalary (){
+    return salary;
+}
+public void setSalary (double newSalary){
+    if (newSalary >= 0.0) {
+        salary = newSalary;
+    }
+}
+public double computePay(){
+    System.out.println("Menghitung Pembayaran Gaji Untuk " + getName());
+    return salary/52;
+  }
+
 }
