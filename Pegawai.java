304fff952d37082f23de7c8002adcc65a5ae707a
syadzaintanbenya <91009370+syadzaintanbenya@users.noreply.github.com>
2022-11-09 23:08:54 +0700

    Update Pegawai.java

--- Pegawai.java
+++ Pegawai.java
@@ -1,16 +1,19 @@
+package tugas1;
+
 public class Pegawai {
 
     private String name;
     private String address;
     private int number;
     public Pegawai (String name, String address, int number) {
-      System.out.println("Menyusun Pegawai");
+      System.out.println("Menyusun Pegawai");/* kenapa ini akan muncul dua kali saat di run, karena di class pegawai ini akan diturunkan/extends 
+      atau diwariskan ke class gaji, maka class pegawai ini akan tetap ikut diakses dikelas gaji */
       this.name = name;
       this.address = address;
       this.number = number;
-    }  
-  public void mailCheck () {
-      System.out.println(" memeriksa surat untuk" + this.name + "" + this.address);
+  }  
+  public void mailCheck () { //Polymorphisme atau memiliki input sama dengan output yang berbeda dengan class gaji
+      System.out.println(" Memeriksa surat untuk" + this.name + "" + this.address);
   }
   public String toString () {
       return name + "" + address + "" + number;
@@ -18,7 +21,7 @@ public class Pegawai {
   public String getName (){
       return name;
   }
-  public String gerAddress (){
+  public String getAddress (){
       return address;
   }
   public void setAddress (String newAddress){
@@ -27,5 +30,4 @@ public class Pegawai {
   public int getNumber(){
       return number;
   }
-  }
-  
+}
